59fe93371e8bdba13229e12ff6f021725ebc90a5
Panov Alexey <alexeykapanov@gmail.com>
2019-11-30 20:18:00 +0300

    Add exceptions for generic errors and for invalid arguments.

---
+++ src/Exceptions/GeoLocationAddressException.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CodeblogPro\GeoLocationAddress\Exceptions;
+
+/**
+ * This class represents a generic error.
+ */
+class GeoLocationAddressException extends \Exception
+{
+}
---
+++ src/Exceptions/InvalidArgumentException.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CodeblogPro\GeoLocationAddress\Exceptions;
+
+class InvalidArgumentException extends GeoLocationAddressException
+{
+
+}

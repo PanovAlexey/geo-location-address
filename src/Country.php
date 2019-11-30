26cb0222caf84efe775edc29c0bfcb1fd1c843d4
Panov Alexey <alexeykapanov@gmail.com>
2019-11-30 20:41:00 +0300

    Add model for country entity.

---
+++ src/Country.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CodeblogPro\GeoLocationAddress;
+
+use CodeblogPro\GeoLocationAddress\Exceptions\InvalidArgumentException;
+
+class Country
+{
+    private string $name;
+    private ?string $code = null;
+
+    public function __construct(string $name = null, string $code = null)
+    {
+        if (null === $name) {
+            throw new InvalidArgumentException('A country must have either a name.');
+        }
+
+        $this->name = $name;
+        $this->code = $code;
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function getCode()
+    {
+        return $this->code;
+    }
+
+    public function __toString(): string
+    {
+        return $this->getName()
+        . !empty($this->getCode())
+            ? '(' . $this->getCode() . ')'
+            : '';
+    }
+}
